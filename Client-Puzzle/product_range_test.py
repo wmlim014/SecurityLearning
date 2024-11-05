fa5537d4803a1f5b33606886ae5fe6d30f94afb7
wmlim014 <166711243+wmlim014@users.noreply.github.com>
2024-11-06 01:31:10 +0800

    Update product_range_test.py

--- Client-Puzzle/product_range_test.py
+++ Client-Puzzle/product_range_test.py
@@ -57,22 +57,30 @@ for j in product(range(min, max + 1), repeat = time_repeat):
 '''
 
 # Test with each expected hash (E)
-num_sub_puzzles = 4
-k = 6
+num_sub_puzzles = 3
+k = 4
 worst_hashes = num_sub_puzzles * (k ** 2) # Worst Expected Hashes (WE) = number of sub-puzzle * (2^k)
-total_combinations = 0
 
 print(f"Worst Expected Hashes: {worst_hashes}")
 
 # Loop for each hashes
+ALL_COMBINATIONS = set()
 for i in range(num_sub_puzzles, worst_hashes + 1):
     no_of_combinations = 0    # Reset number of possible combination for current hash
 
-    for j in product(range(1, k + 1), repeat = num_sub_puzzles):
-        no_of_combinations += 1
-        print(j)
+    if i <= (k + 1):
+        combinations = product(range(1, (i + 1)), repeat = num_sub_puzzles)
 
-    print(f"No.of possible combinations [Frequency (F)] for {i} hashes: {no_of_combinations}")
-    total_combinations += no_of_combinations
+    else:
+        combinations = product(range(1, (k + 1)), repeat = num_sub_puzzles)
 
-print(f"Total possible combinations [Total Frequency (F)]: {total_combinations}")
+    for combo in combinations:
+        # Ensure unique combinations are added only once
+        if combo not in ALL_COMBINATIONS:
+            print(combo)
+            ALL_COMBINATIONS.add(combo)
+            no_of_combinations += 1
+
+    print(f"\nNo.of possible combinations [Frequency (F)] for {i} hashes: {no_of_combinations}")
+    print("--------------------------------")
+print(f"Total possible combinations [Total Frequency (F)]: {len(ALL_COMBINATIONS)}\n")
